7cb532e269a0e328dbc2b1197539db8330bcae5b
avgupt <avishig012@gmail.com>
2020-09-04 21:29:15 +0530

    Get patient name and id

--- assignment1.java
+++ assignment1.java
@@ -34,6 +34,14 @@ class Patient {
         return recovery_days;
     }
 
+    int get_id() {
+        return id;
+    }
+
+    String get_name() {
+        return name;
+    }
+
     void display_details() {
         System.out.println(name);
         System.out.println("Temperature is " + body_temp);
